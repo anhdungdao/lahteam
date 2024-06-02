445d13c1ff5c4d4e9434c967f003f97f576a35c8
anhdungdao <135845047+anhdungdao@users.noreply.github.com>
2024-06-02 13:22:47 +0000

    update js

--- js/homeJs.js
+++ js/homeJs.js
@@ -21,37 +21,20 @@ const scrollRightButton = document.getElementById("btNext");
       });
 
       
-// const hightlight = document.getElementById("texth5");
-//       // console.log(hightlight);
-       
-// const messages = [
-//         "Đội ngũ chuyên gia hàng đầu Việt Nam về AI tạo sinh"
-//       ];
-       
-//       let messLength = messages.length;
-       
-//       // console.log(messLength);
-//       let messIndex = 0;
-//       let charIndex = 0;
-       
-//       function typing(messIndex) {
-//         let currentMess = messages[messIndex];
-//         if (charIndex < currentMess.length) {
-//           let currentChar = currentMess.substring(0, charIndex + 1);
-//           // console.log(currentChar);
-//           hightlight.textContent = currentChar;
-//           charIndex ++;
-//         }else{
-//           if(messIndex < messLength - 1){
-//               messIndex ++;
-//           }else{
-//               messIndex = 0;
-//           }
-//           charIndex = 0;
-//         }
-//         console.log(currentMess);
-      
-//         setTimeout(typing, 50, messIndex);
-//       }
-       
-//       typing(0);
+const hightlight = document.getElementById("texth5");
+var i=0;
+var txt="Đội ngũ chuyên gia hàng đầu Việt Nam về AI tạo sinh";
+var type="_";
+
+function typing(){
+  if(i<txt.length){
+    hightlight.innerHTML +=txt.charAt(i);
+    i++;
+    setTimeout(typing,70)
+  }
+}
+typing();
+
+
+
+
